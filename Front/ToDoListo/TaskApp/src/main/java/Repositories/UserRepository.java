e0e7690c5127f1cec1eb05659716bc063c8bae9e
Santi Acevedo <117226776+Und3rW0rld@users.noreply.github.com>
2024-04-25 10:13:14 -0500

    Delete Front/ToDoListo/TaskApp directory
    It was in the wrong place

--- Front/ToDoListo/TaskApp/src/main/java/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package Repositories;
-
-import Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
